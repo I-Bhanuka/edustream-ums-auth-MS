9f925e5adb03d61ee69171c7b04ec39dd7467709
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-27 16:40:16 +0530

    feat: add the validation method to call the user client

--- src/main/java/com/example/edustream_ums_auth_MS/client/UserServiceClient.java
+++ src/main/java/com/example/edustream_ums_auth_MS/client/UserServiceClient.java
@@ -1,6 +1,7 @@
 package com.example.edustream_ums_auth_MS.client;
 
 import com.example.edustream_lib_common.responseDTO.ApiResponse;
+import com.example.edustream_ums_auth_MS.dto.requestDTO.LoginRequestDTO;
 import com.example.edustream_ums_user_MS.dto.requestDTO.GetUserByUsernameRequestDTO;
 import com.example.edustream_ums_user_MS.dto.responseDTO.UserResponseDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,9 @@ public class UserServiceClient {
     @Value("${services.user.api.getByUsername}")
     private String getUserByUsernameEndpoint;
 
+    @Value("${services.user.api.validateCredentials}")
+    private String validateUserCredentialsEndpoint;
+
     public UserServiceClient(@Qualifier("userRestClient") RestClient restClient) {
         this.restClient = restClient;
     }
@@ -34,6 +38,19 @@ public class UserServiceClient {
         return restClient.post()
                 .uri(getUserByUsernameEndpoint)
                 .body(getUserByUsernameRequestDTO)
+                .retrieve()
+                .body( new ParameterizedTypeReference<>() {
+        });
+
+    }
+
+    public ApiResponse<UserResponseDTO> validateUserCredentials(LoginRequestDTO loginRequestDTO) {
+        log.info("Calling the User Service's {} URI to validate user credentials for username: {}",
+                validateUserCredentialsEndpoint, loginRequestDTO.getUsername());
+
+        return restClient.post()
+                .uri(validateUserCredentialsEndpoint)
+                .body(loginRequestDTO)
                 .retrieve()
                 .body( new ParameterizedTypeReference<>() {
         });
