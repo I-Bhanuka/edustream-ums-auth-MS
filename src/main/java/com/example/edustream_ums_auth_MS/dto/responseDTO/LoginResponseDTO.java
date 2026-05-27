f34236dfcfded746f8931cbe92cbf8013e8ce50e
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-27 16:11:48 +0530

    feat: add DTOs for login request and response

---
+++ src/main/java/com/example/edustream_ums_auth_MS/dto/responseDTO/LoginResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.edustream_ums_auth_MS.dto.responseDTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class LoginResponseDTO {
+
+    private String username;
+
+    private String role;
+}
