42cfeaae90f5f0b46fbe8713cba90197672ac27a
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-27 16:09:26 +0530

    feat: add UserServiceClient implementaion to call the user service

---
+++ src/main/java/com/example/edustream_ums_auth_MS/client/UserServiceClient.java
@@ -0,0 +1,42 @@
+package com.example.edustream_ums_auth_MS.client;
+
+import com.example.edustream_lib_common.responseDTO.ApiResponse;
+import com.example.edustream_ums_user_MS.dto.requestDTO.GetUserByUsernameRequestDTO;
+import com.example.edustream_ums_user_MS.dto.responseDTO.UserResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@Slf4j
+public class UserServiceClient {
+
+    private final RestClient restClient;
+
+    // The endpoints
+    @Value("${services.user.api.create}")
+    private String createUserEndpoint;
+
+    @Value("${services.user.api.getByUsername}")
+    private String getUserByUsernameEndpoint;
+
+    public UserServiceClient(@Qualifier("userRestClient") RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public ApiResponse<UserResponseDTO> getByUsername(GetUserByUsernameRequestDTO getUserByUsernameRequestDTO) {
+        log.info("Calling the User Service's {} URI to get user details for username: {}",
+                getUserByUsernameEndpoint, getUserByUsernameRequestDTO.getUsername());
+
+        return restClient.post()
+                .uri(getUserByUsernameEndpoint)
+                .body(getUserByUsernameRequestDTO)
+                .retrieve()
+                .body( new ParameterizedTypeReference<>() {
+        });
+
+    }
+}
