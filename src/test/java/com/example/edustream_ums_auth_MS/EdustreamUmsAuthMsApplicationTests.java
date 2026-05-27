05bfac33a46e5f027fb09c76f4596f2af82404b3
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-27 13:13:29 +0530

    intial commit

---
+++ src/test/java/com/example/edustream_ums_auth_MS/EdustreamUmsAuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edustream_ums_auth_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdustreamUmsAuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
