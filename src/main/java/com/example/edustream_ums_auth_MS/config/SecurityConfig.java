e877e4aba6d79cd1fca1c9fcf0f711eabb97aef6
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-28 14:42:46 +0530

    feat: add access denied exception handler and authentication required exception handler into security config

--- src/main/java/com/example/edustream_ums_auth_MS/config/SecurityConfig.java
+++ src/main/java/com/example/edustream_ums_auth_MS/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.edustream_ums_auth_MS.config;
 
+import com.example.edustream_lib_security.exception.CustomAccessDeniedHandler;
+import com.example.edustream_lib_security.exception.CustomAuthenticationEntryPoint;
 import com.example.edustream_lib_security.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -20,12 +22,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // Setup
                 .csrf(csrf -> csrf.disable())                  // Disable CSRF because we do not have a website yet
+
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))        // We do not create sessions, we are stateless because of JWT
+
+                // Exceptions
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                        .accessDeniedHandler(new CustomAccessDeniedHandler())
+                )
+
+                // Rules
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login").permitAll()               // Rule 1: Allow unauthenticated access to /auth/** endpoints
                         .anyRequest().authenticated())                                  // Rule 2: Any other request must be authenticated
+
+                // Filter Override
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);// Add before the default UsernamePasswordAuthenticationFilter
 
         return http.build();
